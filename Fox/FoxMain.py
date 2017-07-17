589e1af6446d5f6bf79a7faa2e2d521faaa08a58
Bobloy <alboblexloy@gmail.com>
2017-07-17 11:14:40 -0400

    FoxMain -> Fox

---
+++ Fox/FoxMain.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class Fox:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def fox(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+
+
+    @fox.commands()
+    async def punch(self, user : discord.Member):
+        """I will puch anyone! >.<"""
+        #Your code will go here
+        await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+
+def setup(bot):
+    bot.add_cog(FoxMain(bot))
