b07a49ea716ed49c7b57ad394f36fbb00f87d68f
Bobloy <alboblexloy@gmail.com>
2017-07-17 10:58:54 -0400

    Sub-command practice

--- FoxMain/FoxMain.py
+++ FoxMain/FoxMain.py
@@ -8,11 +8,19 @@ class FoxMain:
         self.bot = bot
 
     @commands.command()
-    async def mycom(self):
+    async def fox(self):
         """This does stuff!"""
 
         #Your code will go here
         await self.bot.say("I can do stuff!")
+
+
+    @fox.commands()
+        async def punch(self, user : discord.Member):
+        """I will puch anyone! >.<"""
+
+        #Your code will go here
+        await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
 
 def setup(bot):
     bot.add_cog(FoxMain(bot))
