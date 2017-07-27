5f1b827243c6db6f69b52d7b8694e39f63be18d9
Bobloy <alboblexloy@gmail.com>
2017-07-27 09:57:25 -0400

    Soup and hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -6,12 +6,113 @@ from discord.ext import commands
 from .utils.dataIO import dataIO
 from .utils import checks
 
+try: # check if BeautifulSoup4 is installed
+	from bs4 import BeautifulSoup
+	soupAvailable = True
+except:
+	soupAvailable = False
+
+import aiohttp
+
 class hangman:
     def __init__(self, bot):
         self.bot = bot
         self.path = "data/Fox-Cogs/hangman"
         self.file_path = "data/Fox-Cogs/hangman/hangman.json"
         self.the_data = dataIO.load_json(self.file_path)
+        self.hangman = (
+        """
+            _________
+            |/        
+            |              
+            |                
+            |                 
+            |               
+            |                   
+            |___                 
+        """,
+
+        """
+           _________
+            |/   |      
+            |              
+            |                
+            |                 
+            |               
+            |                   
+            |___                 
+            H""",
+
+        """
+           _________       
+            |/   |              
+            |   (_)
+            |                         
+            |                       
+            |                         
+            |                          
+            |___                       
+            HA""",
+
+        """
+           ________               
+            |/   |                   
+            |   (_)                  
+            |    |                     
+            |    |                    
+            |                           
+            |                            
+            |___                    
+            HAN""",
+
+
+        """
+           _________             
+            |/   |               
+            |   (_)                   
+            |   /|                     
+            |    |                    
+            |                        
+            |                          
+            |___                          
+            HANG""",
+
+
+        """
+           _________              
+            |/   |                     
+            |   (_)                     
+            |   /|\                    
+            |    |                       
+            |                             
+            |                            
+            |___                          
+            HANGM""",
+
+
+
+        """
+           ________                   
+            |/   |                         
+            |   (_)                      
+            |   /|\                             
+            |    |                          
+            |   /                            
+            |                                  
+            |___                              
+            HANGMA""",
+
+
+        """
+           ________
+            |/   |     
+            |   (_)    
+            |   /|\           
+            |    |        
+            |   / \        
+            |               
+            |___           
+            HANGMAN""")
 
     def save_data(self):
         dataIO.save_json(self.file_path, self.the_data)
@@ -21,11 +122,13 @@ class hangman:
         """Play a game of hangman against the bot!"""
         if guess is None:
             if self.the_data["running"] == True:
-                await self.bot.say("Game of hangman is already running!\n Enter your guess!")
+                await self.bot.say("Game of hangman is already running!\nEnter your guess!")
                 """await self.bot.send_cmd_help(ctx)"""
             else:
-                await self._startgame()
                 await self.bot.say("Starting a game of hangman!")
+                await self._startgame()
+                await self._printgame()
+                
         else:
             await self.bot.say("A game of hangman is now stopping!")
             await self._stopgame()
@@ -45,7 +148,11 @@ class hangman:
         
     async def _startgame(self):
         self.the_data["running"] = True
+        self.the_data["hangman"] = 0
         self.save_data()
+        
+        #self._getphrase()
+        #self._printgame()
         
     async def _stopgame(self):
         self.the_data["running"] = False
@@ -53,9 +160,24 @@ class hangman:
         
     async def _getphrase(self):
         '''Get a new phrase for the game'''
-        
+        url = "https://www.playstationtrophies.org/forum/wheel-of-fortune-2012-/176009-wheel-fortune-answer-list.html"
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
+            await self.bot.say(online + ' players are playing this game at the moment')
+        except:
+            await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
+    
     async def _guessletter(self):
         '''Checks the guess on a letter'''
+        
+    async def _printgame(self):
+        '''Print the current state of game'''
+        await self.bot.say(self.hangman[the_data["hangman"]])
+        self.the_data["hangman"] += 1
+        self.save_data()
+        
     
 def check_folders():
     if not os.path.exists("data/Fox-Cogs"):
@@ -69,9 +191,14 @@ def check_folders():
         
 def check_files():
     if not dataIO.is_valid_json("data/Fox-Cogs/hangman/hangman.json"):
-        dataIO.save_json("data/Fox-Cogs/hangman/hangman.json" ,{"running" : False })
+        dataIO.save_json("data/Fox-Cogs/hangman/hangman.json" ,{"running" : False, "hangman" : 0 })
 
+        
 def setup(bot):
     check_folders()
     check_files()
-    bot.add_cog(hangman(bot))
+    if soupAvailable:
+        bot.add_cog(hangman(bot))
+    else:
+		raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
+        
