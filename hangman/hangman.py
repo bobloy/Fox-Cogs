f64ecf81f49403506f51a53ff991244283592aaa
Bobloy <alboblexloy@gmail.com>
2017-07-27 10:38:31 -0400

    shorter

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -20,7 +20,9 @@ class hangman:
         self.path = "data/Fox-Cogs/hangman"
         self.file_path = "data/Fox-Cogs/hangman/hangman.json"
         self.the_data = dataIO.load_json(self.file_path)
-        self.hangman = (
+        self.hangman = ( "","H","HA","HAN","HANG","HANGM","HANGMA","HANGMAN")
+        
+        '''self.hangman = (
         """
             _________
             |/        
@@ -30,7 +32,7 @@ class hangman:
             |               
             |                   
             |___                 
-        """,
+            """,
 
         """
            _________
@@ -112,7 +114,8 @@ class hangman:
             |   / \        
             |               
             |___           
-            HANGMAN""")
+            HANGMAN""")'''
+            
 
     def save_data(self):
         dataIO.save_json(self.file_path, self.the_data)
