c9cc8a2489169d37e83631462426419a6968bead
Bobloy <alboblexloy@gmail.com>
2017-08-04 16:15:25 -0400

    no async

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -18,8 +18,8 @@ class hangtest:
         self.bot = bot
     
     @commands.command(aliases=['h'], pass_context=True)
-    async def hangtest(self, ctx, guess : str=None):
-        await os.remove("data/Fox-Cogs/hangman/hanganswers.txt")
+    def hangtest(self, ctx, guess : str=None):
+        os.remove("data/Fox-Cogs/hangman/hanganswers.txt")
 
 class hangman:
     def __init__(self, bot):
