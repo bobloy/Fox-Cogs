52bc1a1d176433f5302dff0f4a74a88be7309580
Bobloy <alboblexloy@gmail.com>
2017-08-08 14:57:39 -0400

    spaces

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -256,7 +256,7 @@ def check_folders():
         
 def check_files():
     #if not dataIO.is_valid_json("data/Fox-Cogs/hangman/hangman.json"):
-    dataIO.save_json("data/Fox-Cogs/hangman/hangman.json", {"running": False, "hangman": 0, "theface": "<:never:336861463446814720>"})
+    dataIO.save_json("data/Fox-Cogs/hangman/hangman.json", {"running": False, "hangman": 0, "guesses"=[], "theface": "<:never:336861463446814720>"})
     
 
 def setup(bot):
