d1e5d7b1c3c559129af6da2c9e5aa23119f246a1
Bobloy <alboblexloy@gmail.com>
2017-08-14 16:02:30 -0400

    Don't overwrite json on reload

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -258,8 +258,8 @@ def check_folders():
 
         
 def check_files():
-    #if not dataIO.is_valid_json("data/Fox-Cogs/hangman/hangman.json"):
-    dataIO.save_json("data/Fox-Cogs/hangman/hangman.json", {"running": False, "hangman": 0, "guesses": [], "theface": "<:never:336861463446814720>"})
+    if not dataIO.is_valid_json("data/Fox-Cogs/hangman/hangman.json"):
+        dataIO.save_json("data/Fox-Cogs/hangman/hangman.json", {"running": False, "hangman": 0, "guesses": [], "theface": "<:never:336861463446814720>"})
     
 
 def setup(bot):
