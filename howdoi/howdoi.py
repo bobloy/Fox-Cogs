dd9047d08eada78669ddfdb4893727e4436b267b
Bobloy <alboblexloy@gmail.com>
2018-02-05 14:51:25 -0500

    Howdoi beta

---
+++ howdoi/howdoi.py
@@ -0,0 +1,34 @@
+import discord
+
+from discord.ext import commands
+
+from .utils.chat_formatting import pagify
+from .utils.chat_formatting import box
+
+from howdoi import howdoi
+
+
+class Howdoi:
+    """Cog for answering coding questions"""
+
+    def __init__(self, bot):
+        self.query = ""
+        self.args = {
+            "query": self.query
+            }
+
+    @commands.command(pass_context=True)
+    async def howdoi(self, ctx, *question):
+        """Ask a coding question"""
+        self.query = " ".join(question)
+        
+        self.args["query"] = self.query
+        
+        out = howdoi.howdoi(self.args)
+        
+        for page in pagify(out, shorten_by=24):
+            await self.bot.say(box(page))
+        
+def setup(bot):
+    n = Howdoi(bot)
+    bot.add_cog(n)
