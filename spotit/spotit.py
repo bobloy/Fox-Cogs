9e774b64feb68a2f3f32d0bab7c341095effba4e
Bobloy <alboblexloy@gmail.com>
2018-02-16 15:07:59 -0500

    spotit continued

--- spotit/spotit.py
+++ spotit/spotit.py
@@ -2,12 +2,15 @@ import discord
 import os
 
 from discord.ext import commands
-from random import randint
+from random import randint, shuffle
 
 from .utils.dataIO import dataIO
 from .utils import checks
 
 PRIME_LIST = [0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+U_LETTERS = "🇦🇧🇨🇩🇪🇫🇬🇭🇮🇯🇰🇱🇲🇳🇴🇵🇶🇷🇸🇹🇺🇻🇼🇽🇾🇿"
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 
 class Spotit:
     """Lets anyone play a game of hangman with custom phrases"""
@@ -21,20 +24,78 @@ class Spotit:
         self.cardlist = [] 
         self.is_running = False
         self.emojicount = 0
-    
+        self.leftcard = None
+        self.rightcard = None
+        self.answer = None
+        self.answer_emoji = None
+
+
+    async def pick_a_card(self, channel, user_scores):
+        shuffle(self.cardlist)
+
+        self.leftcard = self.cardlist.pop()
+        self.rightcard = self.cardlist[-1]
+
+        embed = self._card_embeds()
+        
+        await self.bot.send_message(channel, embed=embed)
+        
+        response = await self.bot.wait_for_message(timeout=15, channel=channel, content=self.answer)
+        
+        if not reponse:
+            await self.bot.send_message(channel, "Timed-out! Ending game")
+            await self._stopgame()
+        else:
+            await self.bot.send_message(channel, "Correct! Answer was {}:{}".format(self.answer, str(self.answer_emoji))
+
+    def _card_embeds(self):
+        embed=discord.Embed(title="Spot-It!", description="Identify the matching symbols!")
+     
+        card1 = [self.emojilist[x-1] for x in self.leftcard]
+        card2 = [self.emojilist[x-1] for x in self.rightcard]
+        
+        rev_u_letters = list(U_LETTERS[::-1])  # Reverse u_letters as a list
+        rev_letters = list(LETTERS[::-1])  # Reverse letters as a list
+
+        self.answer = self.check_cards(self.leftcard, self.rightcard)
+        
+        text1 = ""
+        text2 = ""
+        
+        for x in range(len(card1)):
+            if x%3 = 0:  # New line
+                text1 += "\n"+rev_u_letters.pop()
+                text2 += "\n⏹"
+                rev_letters.pop()
+            
+            if self.leftcard[x] = self.answer:
+                self.answer = rev_letters[-1]+"123"[x%3]
+                self.answer_emoji = card1[x]
+            
+            text1 += str(card1[x])
+            text2 += str(card2[x])
+            
+        text1 += "\n⏹:one::two::three:"
+        text2 += "\n⏹⏹⏹⏹"
+        
+
+        embed.add_field(name=Card 1, value=text1, inline=True)
+        embed.add_field(name=Card 2, value=text2, inline=True)
+        
+
     def new_game():
         self.emojilist = self.load_emojis()
-        
+
         if not self.emojilist:
             print("Not enough custom emojis")
             return False
-        
+
         for x in range(len(PRIME_LIST)):
             p = PRIME_LIST[x]
             if p * p + p + 1 > len(self.load_emojis()):
                 self.cardslist, self.emojicount = self.create_cards(PRIME_LIST[x-1])
                 return True
-        
+
         print("How the hell do you have so many emojis available to you?")
         return False
 
@@ -56,7 +117,7 @@ class Spotit:
         return cards, p * p + p + 1
 
     def check_cards(card1, card2):
-        return sorted(card & other_card)
+        return sorted(card1 & card2)
               
     def save_data(self):
         """Saves the json"""
@@ -73,25 +134,7 @@ class Spotit:
         if ctx.invoked_subcommand is None:
             await self.bot.send_cmd_help(ctx)
             
-    @spotitset.command(pass_context=True)
-    async def servers(self, ctx):
-        message = ctx.message
-        #Borrowing FlapJack's emoji validation (https://github.com/flapjax/FlapJack-Cogs/blob/master/smartreact/smartreact.py)
-        if theface[:2] == "<:":
-            theface = [r for server in self.bot.servers for r in server.emojis if r.id == theface.split(':')[2][:-1]][0]
-        
-        try:
-            # Use the face as reaction to see if it's valid (THANKS FLAPJACK <3)
-            await self.bot.add_reaction(message, theface)
 
-        except discord.errors.HTTPException:
-            await self.bot.say("That's not an emoji I recognize.")
-        else:
-            self.the_data["theface"] = str(theface)
-            self.save_data()
-            self._updateHanglist()
-            await self.bot.say("Face has been updated!")
-            
     @commands.command(pass_context=True)
     async def spotit(self, ctx):
         """Start a new game of Spot-It"""
@@ -101,7 +144,7 @@ class Spotit:
         
         user_scores = {}
         
-        await self.pick_a_card(ctx, user_scores)
+        await self._startgame(ctx.message.channel, user_scores)
         
         if user_scores: # If anyone responded at all
             await self.bot.say("Here are the scores!")
@@ -119,152 +162,19 @@ class Spotit:
         self._stopgame()
         await self.bot.say("Game has been abandoned..")
     
-    def _startgame(self):
+    def _startgame(self, channel, user_scores):
         """Starts a new game of hangman"""
-        self.the_data["answer"] = self._getphrase().upper()
-        self.the_data["hangman"] = 0
-        self.the_data["guesses"] = []
-        self.winbool = False
-        self.the_data["running"] = True
-        self.the_data["trackmessage"] = False
-        self.save_data()
+        self.is_running= True
+        
+        while self.is_running:  # Until someone stops it or times out or winner
+            await self.pick_a_card(channel, user_scores)
+        
+
         
     def _stopgame(self):
         """Stops the game in current state"""
-        self.the_data["running"] = False
-        self.save_data()
-    
-    async def _checkdone(self, channel=None):
-        if self.winbool:
-            if channel:
-                await self.bot.send_message(channel, "You Win!")
-            else:
-                await self.bot.say("You Win!")
-            self._stopgame()
-                
-        if self.the_data["hangman"] >= 7:
-            if channel:
-                await self.bot.send_message(channel, "You Lose!\nThe Answer was: **"+self.the_data["answer"]+"**")
-            else:
-                await self.bot.say("You Lose!\nThe Answer was: **"+self.the_data["answer"]+"**")
-                
-            self._stopgame()
-                
-    def _getphrase(self):
-        """Get a new phrase for the game and returns it"""
-        phrasefile = open(self.answer_path, 'r')
-        phrases = phrasefile.readlines()
-        
-        outphrase = ""
-        while outphrase == "":
-            outphrase = phrases[randint(0, len(phrases)-1)].partition(" (")[0]
-#           outphrase = phrases[randint(0,10)].partition(" (")[0]
-        return outphrase
-   
-    def _hideanswer(self):
-        """Returns the obscured answer"""
-        out_str = ""
-        
-        self.winbool = True
-        for i in self.the_data["answer"]:
-            if i == " " or i == "-":
-                out_str += i*2
-            elif i in self.the_data["guesses"] or i not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-                out_str += "__"+i+"__ "
-            else:
-                out_str += "**\_** "
-                self.winbool = False
-                
-        return out_str
-        
-    def _guesslist(self):
-        """Returns the current letter list"""
-        out_str = ""
-        for i in self.the_data["guesses"]:
-            out_str += str(i) + ","
-        out_str = out_str[:-1]
-        
-        return out_str
-        
-    async def _guessletter(self, guess, channel=None):
-        """Checks the guess on a letter and prints game if acceptable guess"""
-        if not guess.upper() in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or not len(guess) == 1:
-            if channel:
-                await self.bot.send_message(channel, "Invalid guess. Only A-Z is accepted")
-            else:
-                await self.bot.say("Invalid guess. Only A-Z is accepted")
-            return
+        self.is_running = False
 
-        if guess.upper() in self.the_data["guesses"]:
-            if channel:
-                await self.bot.send_message(channel, "Already guessed that! Try again")
-            else:
-                await self.bot.say("Already guessed that! Try again")
-            return
-
-        if not guess.upper() in self.the_data["answer"]:
-            self.the_data["hangman"] += 1
-            
-        self.the_data["guesses"].append(guess.upper())
-        self.save_data()
-        
-        await self._printgame(channel)
-        
-    async def _on_react(self, reaction, user):
-        """ Thanks to flapjack reactpoll for guidelines
-            https://github.com/flapjax/FlapJack-Cogs/blob/master/reactpoll/reactpoll.py"""
-        
-        
-        
-        if not self.the_data["trackmessage"]:
-            return
-        
-        if user == self.bot.user:
-            return  # Don't remove bot's own reactions
-        message = reaction.message
-        emoji = reaction.emoji
-        
-        if not message.id == self.the_data["trackmessage"]:
-            return
-        
-        if str(emoji) in self.letters:
-            letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[self.letters.index(str(emoji))]
-            await self._guessletter(letter, message.channel)
-            
-            
-        if str(emoji) in self.navigate:
-            if str(emoji) == self.navigate[0]:
-                await self._reactmessage_am(message)
-            
-            if str(emoji) == self.navigate[-1]:
-                await self._reactmessage_nz(message)
-    
-    
-    async def _reactmessage_menu(self, message):
-        """React with menu options"""
-        await self.bot.clear_reactions(message)
-        
-        await self.bot.add_reaction(message, self.navigate[0])
-        await self.bot.add_reaction(message, self.navigate[-1])
-        
-    async def _reactmessage_am(self, message):
-        await self.bot.clear_reactions(message)
-
-        for x in range(len(self.letters)):
-            if x in [i for i,b in enumerate("ABCDEFGHIJKLM") if b not in self._guesslist()]:
-                await self.bot.add_reaction(message, self.letters[x])
-         
-        await self.bot.add_reaction(message, self.navigate[-1])
-
-
-    async def _reactmessage_nz(self, message):
-        await self.bot.clear_reactions(message)
-
-        for x in range(len(self.letters)):
-            if x in [i for i,b in enumerate("NOPQRSTUVWXYZ") if b not in self._guesslist()]:
-                await self.bot.add_reaction(message, self.letters[x+13])
-                
-        await self.bot.add_reaction(message, self.navigate[0])        
 
 
     async def _printgame(self, channel=None):
